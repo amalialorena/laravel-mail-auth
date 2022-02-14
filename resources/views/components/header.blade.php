d8b15e367d102917b74bc0a742e5df4ae9477cb0
Amalia <amalialorenaferariu@gmail.com>
2022-02-14 20:37:06 +0100

    added login

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,23 +1,32 @@
 <header>
     @auth
-        <a href="{{route('logout')}}" class="btn btn-primary">Logout</a>
-   
-    @else
-Register:
-    <form action="{{ route('register') }}" method='POST'>
-        @method('POST')
-        @csrf
-        <label for="name">Name</label> <br>
-        <input type="text" name="name"><br>
-        <label for="email">Email</label><br>
-        <input type="text" name="email"><br>
-        <label for="password">Password</label><br>
-        <input type="password" name="password"><br>
-        <label for="password_confirmation">Password confirmation</label><br>
-        <input type="password" name="password_confirmation"><br> <br>
+        <br>
+        <a href="{{ route('logout') }}" class="btn btn-primary">Logout</a>
 
-        <input type="submit" name="register" value="Register">
-    </form>
+    @else
+        Register:
+        <form action="{{ route('register') }}" method='POST'>
+            @method('POST')
+            @csrf
+            <label for="name">Name</label> <br>
+            <input type="text" name="name"><br>
+            <label for="email">Email</label><br>
+            <input type="text" name="email"><br>
+            <label for="password">Password</label><br>
+            <input type="password" name="password"><br>
+            <label for="password_confirmation">Password confirmation</label><br>
+            <input type="password" name="password_confirmation"><br> <br>
+            <input type="submit" name="register" value="Register">
+        </form>
+
+        <form action="{{ route('login') }}" method='POST'>
+            @method('POST')
+            @csrf
+            <label for="email">Email</label><br>
+            <input type="text" name="email"><br>
+            <label for="password">Password</label><br>
+            <input type="password" name="password"><br> <br>
+            <input type="submit" name="register" value="Login"> <br>
+        </form>
     @endauth
 </header>
-
